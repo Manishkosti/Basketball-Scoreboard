4426ea8edacf34303edfb115fdabebe5c8a74248
Manish Kosti <manishkosti.13@gmail.com>
2024-07-12 01:00:38 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let homeEl = document.getElementById("home-el")
+let home = 0
+
+function increment1() {
+    home += 1
+    homeEl.textContent = home
+}
+
+function increment2() {
+    home += 2
+    homeEl.textContent = home
+}
+
+function increment3() {
+    home += 3
+    homeEl.textContent = home
+}
+
+let awayEl = document.getElementById("away-el")
+let away = 0
+
+function increment11() {
+    away += 1
+    awayEl.textContent = away
+}
+
+function increment22() {
+    away += 2
+    awayEl.textContent = away
+}
+
+function increment33() {
+    away += 3
+    awayEl.textContent = away
+}
